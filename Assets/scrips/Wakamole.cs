f89ec66f42020084ba8b065327229505e2d6e5c8
adridas16 <adridas666@gmail.com>
2024-11-06 14:17:18 +0100

    wack a a mola

--- Assets/scrips/Wakamole.cs
+++ Assets/scrips/Wakamole.cs
@@ -4,7 +4,14 @@ using UnityEngine;
 
 public class Wakamole : MonoBehaviour
 {
+    [SerializeField] private float visibleHeigth = 0.27f;
+    [SerializeField] private float hiddenHeigth = 0.13f;
+    private Vector3 myNewXYZPosition;
     // Start is called before the first frame update
+    private void Awake()
+    {
+        HideMole();
+    }
     void Start()
     {
         
@@ -14,5 +21,9 @@ public class Wakamole : MonoBehaviour
     void Update()
     {
         
+    }
+    public void HideMole()
+    {
+        transform.localPosition=new Vector3(transform.localPosition.x,hiddenHeigth,transform.localPosition.z);
     }
 }
